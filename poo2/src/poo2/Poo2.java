e08b9a52c82fdef4a873c4b759fd5caf6015b1a4
mrp4v3l <mrp4v3l@gmail.com>
2023-02-17 16:19:58 -0500

    mi ca

--- poo2/src/poo2/Poo2.java
+++ poo2/src/poo2/Poo2.java
@@ -12,6 +12,7 @@ public class Poo2 {
     public static void main(String[] args) {
         Circunferencia ra = new Circunferencia();
         ra.crearCircunferencia();
+        
         System.out.println(ra.area());
         System.out.println(ra.perimetro());
         
